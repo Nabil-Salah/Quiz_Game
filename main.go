de1412d710a475092a7b1addf43e3053552e1172
Nabil Salah <nabil.salah203@gmail.com>
2024-04-01 22:13:46 +0200

    Adding timer to qiuz

--- main.go
+++ main.go
@@ -2,22 +2,28 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
-	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
-	contents, err := os.ReadFile("problems.csv")
+	timeLimit := flag.Int("limit", 10, "the time limit for the quiz in seconds")
+	filename := flag.String("fname", "problems.csv", "the time limit for the quiz in seconds")
+	flag.Parse()
+	contents, err := os.ReadFile(*filename)
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
 	}
 	r := csv.NewReader(strings.NewReader(string(contents)))
 	var degree int
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+questionsloop:
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
@@ -27,11 +33,21 @@ func main() {
 			log.Fatal(err)
 		}
 		fmt.Println("What is", record[0], "?")
-		var ans int
-		_, err = fmt.Scanf("%d", &ans)
-		num, err := strconv.Atoi(strings.TrimSpace(record[1]))
-		if num == ans {
-			degree++
+		ansch := make(chan string)
+		go func() {
+			var ans string
+			_, err = fmt.Scanf("%s\n", &ans)
+			ansch <- ans
+		}()
+		select {
+		case <-timer.C:
+			break questionsloop
+		case answer := <-ansch:
+			num := strings.TrimSpace(record[1])
+			answer = strings.TrimSpace(answer)
+			if num == answer {
+				degree++
+			}
 		}
 	}
 	fmt.Printf("your answered %d answers correctly\n", degree)
