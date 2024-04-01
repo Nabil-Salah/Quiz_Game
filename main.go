b29ec47ad7b575ccc023a14143784bc5eff0e812
Nabil Salah <nabil.salah203@gmail.com>
2024-04-01 17:18:08 +0200

    Task 1 finnish

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	contents, err := os.ReadFile("problems.csv")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	r := csv.NewReader(strings.NewReader(string(contents)))
+	var degree int
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("What is", record[0], "?")
+		var ans int
+		_, err = fmt.Scanf("%d", &ans)
+		num, err := strconv.Atoi(strings.TrimSpace(record[1]))
+		if num == ans {
+			degree++
+		}
+	}
+	fmt.Printf("your answered %d answers correctly\n", degree)
 }
